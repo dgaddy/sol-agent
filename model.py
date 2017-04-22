d1f857dde347b35fb6512646bc4de761bae063b8
Nikita Kitaev <nikitakit@gmail.com>
2017-04-21 18:30:27 -0700

    Model and exploration changes

--- model.py
+++ model.py
@@ -1,3 +1,14 @@
+try:
+    import xdbg
+    INTERACTIVE = True
+except:
+    INTERACTIVE = False
+
+if INTERACTIVE:
+    import os
+    os.chdir(os.path.expanduser('~/dev/cards/sol-agent'))
+
+# %%
 import time
 import random
 import pickle
@@ -7,15 +18,19 @@ from collections import defaultdict
 
 import numpy as np
 import tensorflow as tf
+import gym
 import sys
 sys.path.append('../sol-env')
 import sol_env
+
+# %%
 
 class Buffer(object):
     def __init__(self, size):
         self.size = size
         self.items = []
         self.index = 0
+        self.stats = []
 
     def insert(self, item):
         if len(self.items) < self.size:
@@ -25,7 +40,27 @@ class Buffer(object):
             self.index = (self.index + 1) % self.size
 
     def sample(self, k):
-        return random.sample(self.items, k)
+        # return random.sample(self.items, k)
+        res = random.sample(self.items, k)
+        while np.mean([0. if x[3] < 0 else 1. for x in res]) < 0.2:
+            for i in reversed(range(k)):
+                obs, act, next_obs, rew, done = res[i]
+                if rew < 0:
+                    del res[i]
+            res.extend(random.sample(self.items, k - len(res)))
+
+
+        try:
+            self.stats
+        except AttributeError:
+            self.stats = []
+
+        self.stats.append([0. if x[3] < 0 else 1. for x in res])
+        if len(self.stats) >= 100:
+            print('Percent of valid actions sampled:', np.mean(self.stats))
+            self.stats.clear()
+        return res
+
 
 class DebugHelper(object):
     def __init__(self, n_slots, max_stack_len):
@@ -33,6 +68,9 @@ class DebugHelper(object):
         self.index = 0
         self.n_slots = n_slots
         self.max_stack_len = max_stack_len
+
+        self.random_items = []
+        self.random_index = 0
 
     def insert(self, observation, q_values, action, reward):
         size = 100
@@ -42,7 +80,16 @@ class DebugHelper(object):
         else:
             self.items[self.index] = item
             self.index = (self.index + 1) % size
-        
+
+    def insert_random(self, observation, action, reward):
+        size = 100
+        item = (observation, action, reward)
+        if len(self.random_items) < size:
+            self.random_items.append(item)
+        else:
+            self.random_items[self.random_index] = item
+            self.random_index = (self.random_index + 1) % size
+
     def print_analysis(self):
         counts = defaultdict(int)
         for obs, q_values, action, reward in self.items:
@@ -52,10 +99,62 @@ class DebugHelper(object):
 
             counts[(action_type, valid)] += 1
 
+        counts_random = defaultdict(int)
+        for obs, action, reward in self.random_items:
+            action_type = action[0]
+            valid = reward >= 0
+
+            counts_random[(action_type, valid)] += 1
+
+
         print('===========debug info===========')
         for at in sol_env.ActionType:
-            print('valid', at, counts[(at,True)])
-            print('invalid', at, counts[(at,False)])
+            print('invalid', at, counts[(at,False)], '/', counts[(at,True)] + counts[(at,False)])
+        for at in sol_env.ActionType:
+            print('random invalid', at, counts_random[(at,False)], '/', counts_random[(at,True)] + counts_random[(at,False)])
+
+
+
+def batch_gather(params, indices, validate_indices=None,
+    batch_size=None,
+    options_size=None):
+    """
+    Gather slices from `params` according to `indices`, separately for each
+    example in a batch.
+
+    output[b, i, ..., j, :, ..., :] = params[b, indices[b, i, ..., j], :, ..., :]
+
+    The arguments `batch_size` and `options_size`, if provided, are used instead
+    of looking up the shape from the inputs. This may help avoid redundant
+    computation (TODO: figure out if tensorflow's optimizer can do this automatically)
+
+    Args:
+      params: A `Tensor`, [batch_size, options_size, ...]
+      indices: A `Tensor`, [batch_size, ...]
+      validate_indices: An optional `bool`. Defaults to `True`
+      batch_size: (optional) an integer or scalar tensor representing the batch size
+      options_size: (optional) an integer or scalar Tensor representing the number of options to choose from
+    """
+    if batch_size is None:
+        batch_size = params.get_shape()[0].merge_with(indices.get_shape()[0]).value
+        if batch_size is None:
+            batch_size = tf.shape(indices)[0]
+
+    if options_size is None:
+        options_size = params.get_shape()[1].value
+        if options_size is None:
+            options_size = tf.shape(params)[1]
+
+    batch_size_times_options_size = batch_size * options_size
+
+    # TODO(nikita): consider using gather_nd. However as of 1/9/2017 gather_nd
+    # has no gradients implemented.
+    flat_params = tf.reshape(params, tf.concat([[batch_size_times_options_size], tf.shape(params)[2:]], 0))
+
+    indices_offsets = tf.reshape(tf.range(batch_size) * options_size, [-1] + [1] * (len(indices.get_shape())-1))
+    indices_into_flat = indices + tf.cast(indices_offsets, indices.dtype)
+
+    return tf.gather(flat_params, indices_into_flat, validate_indices=validate_indices)
 
 
 def q_func(visible_ph, suit_ph, rank_ph, pos_ph, seq_len_ph, type_ph, n_slots, max_stack_len, scope, reuse=False):
@@ -75,21 +174,39 @@ def q_func(visible_ph, suit_ph, rank_ph, pos_ph, seq_len_ph, type_ph, n_slots, m
 
         rank_emb_matrix = tf.get_variable('rank_emb', [16, emb_size], tf.float32, tf.random_normal_initializer())
         rank_emb = tf.nn.embedding_lookup(rank_emb_matrix, rank_ph)
-        
+
         # XXX is this necessary anymore?
         position_emb_matrix = tf.get_variable('pos_emb', [max_stack_len+1, emb_size], tf.float32, tf.random_normal_initializer())
         pos_emb = tf.nn.embedding_lookup(position_emb_matrix, pos_ph)
 
         card_reps = tf.concat([vis_emb, suit_emb, rank_emb, pos_emb],3)
+        # %break
 
-        # run rnn over cards in each slot
-        card_reps_reshaped = tf.reshape(card_reps, [-1, max_stack_len, emb_size*4])
-        seq_len = tf.reshape(seq_len_ph, [-1])
+        # n_slots
+        # max_stack_len
 
-        outputs, (c,h) = tf.nn.dynamic_rnn(tf.contrib.rnn.LSTMCell(lstm_size), card_reps_reshaped, seq_len, dtype=tf.float32, scope='card_lstm')
-        # state is batch_size*n_slots by lstm_size
+        if True:
+            # TODO(nikita): use this to generate card representations
+            # run rnn over cards in each slot
+            card_reps_reshaped = tf.reshape(card_reps, [-1, max_stack_len, emb_size*4])
+            seq_len = tf.reshape(seq_len_ph, [-1])
 
-        card_slot_rep = tf.reshape(c, [-1, n_slots, lstm_size])
+            outputs, (c,h) = tf.nn.dynamic_rnn(tf.contrib.rnn.LSTMCell(lstm_size, reuse=reuse), card_reps_reshaped, seq_len, dtype=tf.float32, scope='card_lstm')
+            # state is batch_size*n_slots by lstm_size
+
+            card_slot_rep = tf.reshape(c, [-1, n_slots, lstm_size])
+
+        # Actual slot representations: no LSTM for now
+        empty_slot_emb = tf.get_variable('empty_slot_emb', shape=[1, 1, lstm_size], initializer=tf.random_normal_initializer())
+
+        top_card_idx = tf.nn.relu(seq_len_ph - 1)
+        top_card_reps = batch_gather(
+            tf.reshape(card_reps, [-1, int(card_reps.get_shape()[2]), int(card_reps.get_shape()[3])]),
+            tf.reshape(top_card_idx, [-1]))
+        top_card_reps = tf.reshape(top_card_reps, [-1, n_slots, int(card_reps.get_shape()[-1])])
+
+        card_slot_rep = tf.where(tf.expand_dims(seq_len_ph > 0, -1) & tf.ones(tf.shape(top_card_reps), dtype=tf.bool),
+            top_card_reps, empty_slot_emb * tf.ones_like(top_card_reps))
 
         # concat in slot properties
 
@@ -101,7 +218,7 @@ def q_func(visible_ph, suit_ph, rank_ph, pos_ph, seq_len_ph, type_ph, n_slots, m
 
         # run rnn over all the slots
 
-        outputs, (c,h) = tf.nn.dynamic_rnn(tf.contrib.rnn.LSTMCell(lstm_size), slot_reps, dtype=tf.float32, scope='slot_lstm')
+        outputs, (c,h) = tf.nn.dynamic_rnn(tf.contrib.rnn.LSTMCell(lstm_size, reuse=reuse), slot_reps, dtype=tf.float32, scope='slot_lstm')
 
         global_context = c # size batch_size by lstm_size
 
@@ -116,12 +233,35 @@ def q_func(visible_ph, suit_ph, rank_ph, pos_ph, seq_len_ph, type_ph, n_slots, m
 
         click_slot_adv_values = tf.layers.conv1d(slot_hiddens, 1, 1) # size batch_size by n_slots
 
-        from_slot_reps = tf.tile(tf.expand_dims(slot_reps, 2), (1, 1, n_slots, 1))
-        to_slot_reps = tf.tile(tf.expand_dims(slot_reps, 1), (1, n_slots, 1, 1))
+        # better-parametrized drag-and-drop
+        to_slot_reps = tf.expand_dims(tf.expand_dims(slot_reps, 1), 3) # (?, 1, n_slots, 1, ...)
 
-        drag_drop_hiddens = tf.layers.conv2d(tf.concat([from_slot_reps, to_slot_reps],3), hidden_size, 1, activation=tf.nn.relu)
+        # the "from" action is parametrized such that the last card in a slot is 0,
+        # while our observation has the first card at index 0.
+        card_reps_reversed = tf.reshape(
+            tf.reverse_sequence(card_reps_reshaped, seq_len, seq_axis=1, batch_axis=0),
+            tf.shape(card_reps))
+        from_reps = tf.expand_dims(card_reps_reversed, 2)
 
-        drag_drop_adv_values = tf.layers.conv2d(drag_drop_hiddens, max_stack_len, 1)
+        drag_drop_reps = tf.concat([
+            tf.tile(from_reps, (1, 1, n_slots, 1, 1)),
+            tf.tile(to_slot_reps, (1, n_slots, 1, max_stack_len, 1)),
+            # tf.tile(tf.reshape(global_context, [-1, 1, 1, 1, lstm_size]), (1, n_slots, n_slots, max_stack_len, 1))
+            ], -1)
+
+        # from_slot_reps = tf.tile(tf.expand_dims(slot_reps, 2), (1, 1, n_slots, 1))
+        # to_slot_reps = tf.tile(tf.expand_dims(slot_reps, 1), (1, n_slots, 1, 1))
+
+        # drag_drop_hiddens = tf.layers.conv2d(tf.concat([from_slot_reps, to_slot_reps],3), hidden_size, 1, activation=tf.nn.relu)
+
+        # drag_drop_adv_values = tf.layers.conv2d(drag_drop_hiddens, max_stack_len, 1)
+
+        drag_drop_hiddens = tf.layers.dense(drag_drop_reps, hidden_size, activation=tf.nn.relu)
+
+        drag_drop_adv_values = tf.squeeze(tf.layers.dense(drag_drop_hiddens, 1), -1)
+        # note: masking out invalid drags is handled outside the computation graph
+
+        # drag_drop_adv_values
 
         adv_values = tf.concat([tf.squeeze(click_slot_adv_values,axis=[2]),
                    tf.reshape(drag_drop_adv_values, (-1,n_slots*n_slots*max_stack_len))],1)
@@ -139,7 +279,7 @@ class Model(object):
         self.n_slots = n_slots
         self.max_stack_len = max_stack_len
 
-	# placeholders
+        # placeholders
         self.visible_ph = tf.placeholder(tf.int32, [None,n_slots,max_stack_len]) # values are pad,y,n
         self.suit_ph = tf.placeholder(tf.int32, [None,n_slots,max_stack_len]) # values are pad,?,S,C,H,D
         self.rank_ph = tf.placeholder(tf.int32, [None,n_slots, max_stack_len]) # pad,?,1,...,13
@@ -147,7 +287,7 @@ class Model(object):
         self.seq_len_ph = tf.placeholder(tf.int32, [None,n_slots])
         self.type_ph = tf.placeholder(tf.int32, [None,n_slots])
 
-	# placeholders for next timestep
+        # placeholders for next timestep
         self.next_visible_ph = tf.placeholder(tf.int32, [None,n_slots,max_stack_len]) # values are pad,y,n
         self.next_suit_ph = tf.placeholder(tf.int32, [None,n_slots,max_stack_len]) # values are pad,?,S,C,H,D
         self.next_rank_ph = tf.placeholder(tf.int32, [None,n_slots, max_stack_len]) # pad,?,1,...,13
@@ -155,16 +295,16 @@ class Model(object):
         self.next_seq_len_ph = tf.placeholder(tf.int32, [None,n_slots])
         self.next_type_ph = tf.placeholder(tf.int32, [None,n_slots])
 
-	# other placeholders
+        # other placeholders
         self.action_ph = tf.placeholder(tf.int32, [None])
         self.reward_ph = tf.placeholder(tf.float32, [None])
         self.done_mask_ph = tf.placeholder(tf.float32, [None])
 
-	# compute q values
+        # compute q values
         self.q_values = q_func(self.visible_ph, self.suit_ph, self.rank_ph, self.pos_ph, self.seq_len_ph, self.type_ph, n_slots, max_stack_len, 'q_func', False)
         q_func_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='q_func')
 
-	# compute q and target q network values for next timestep
+        # compute q and target q network values for next timestep
         next_q_values = q_func(self.next_visible_ph, self.next_suit_ph, self.next_rank_ph, self.next_pos_ph, self.next_seq_len_ph, self.next_type_ph, n_slots, max_stack_len, 'q_func', reuse=True)
         target_next_q_values = q_func(self.next_visible_ph, self.next_suit_ph, self.next_rank_ph, self.next_pos_ph, self.next_seq_len_ph, self.next_type_ph, n_slots, max_stack_len, 'target_q_func', reuse=False)
         target_q_func_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target_q_func')
@@ -218,7 +358,7 @@ class Model(object):
             actions[i] = act
             rewards[i] = rew
             dones[i] = 1 if done else 0
-            
+
         feed_dict = {self.learning_rate:learning_rate, self.action_ph:actions, self.reward_ph:rewards, self.done_mask_ph:dones}
         feed_dict.update(obs_feed)
         feed_dict.update(next_obs_feed)
@@ -234,7 +374,7 @@ class Model(object):
         rank = np.zeros((n_obs,self.n_slots,self.max_stack_len), dtype=np.int32)
         suit = np.zeros((n_obs,self.n_slots,self.max_stack_len), dtype=np.int32)
         pos = np.zeros((n_obs,self.n_slots,self.max_stack_len), dtype=np.int32)
-        
+
         for obs_num in range(n_obs):
             observation = observations[obs_num]
             assert len(observation) == self.n_slots
@@ -257,7 +397,7 @@ class Model(object):
                     visible[obs_num,slot_num,card_num] = v
                     rank[obs_num,slot_num,card_num] = r
                     suit[obs_num,slot_num,card_num] = s
-                
+
         if next_values:
             return {self.next_visible_ph:visible,self.next_suit_ph:suit, self.next_rank_ph:rank, self.next_pos_ph:pos, self.next_seq_len_ph:lengths, self.next_type_ph:types}
         else:
@@ -276,7 +416,7 @@ class Model(object):
             to_slot = a % n_slots
             a = a // n_slots
             from_slot = a
-            
+
             return sol_env.ActionType.DRAG_DROP, from_slot, card, to_slot
         else:
             assert False # out of range
@@ -321,21 +461,21 @@ def get_initial_sample(buffer_size, env, model, max_steps_per_ep, initial_sample
         with open(sample_file, 'wb') as f:
             pickle.dump(buff, f)
         return buff
-             
+
 def main():
     debug = True
     update_freq = 4
     batch_size = 32
     max_steps = 5000000
-    max_steps_per_ep = 1000
+    max_steps_per_ep = 10000
     buffer_size = 1000000
-    init_eps = .5
-    final_eps = .05
-    final_eps_timestep = 1000000
+    init_eps = 0.99 # .5
+    final_eps = 0.1 # .05
+    final_eps_timestep = 10000000 # 1000000
     target_update_freq = 10000
     max_stack_len = 10
 
-    env = sol_env.SolEnv()
+    env = sol_env.SolEnv('klondike')
     obs = env.reset()
     model = Model(len(obs),max_stack_len)
     buff = get_initial_sample(buffer_size, env, model, max_steps_per_ep)
@@ -345,6 +485,7 @@ def main():
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
+        env = gym.wrappers.Monitor(env, os.path.join('/tmp/sol_vid', "gym"), force=True)
         obs = env.reset()
         tf.global_variables_initializer().run()
 
@@ -370,7 +511,7 @@ def main():
                 q_values -= (1-action_mask)*1000
                 action_id = (q_values+np.random.randn(*q_values.shape)*.01).argmax()
                 random_action = False
-                
+
 
             last_obs = obs
             action = model.action_id_to_action(action_id, obs)
@@ -380,6 +521,8 @@ def main():
             buff.insert((last_obs, action_id, obs, rew, done))
             if not random_action:
                 debug_helper.insert(last_obs, q_values, action, rew)
+            else:
+                debug_helper.insert_random(last_obs, action, rew)
 
             if done or episode_t > max_steps_per_ep:
                 obs = env.reset()
@@ -411,4 +554,7 @@ def main():
                     os.mkdir('models')
                 saver.save(sess, 'models/model', global_step=t)
 
-main()
+# %%
+
+if __name__ == '__main__':
+    main()
